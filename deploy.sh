b56fd73302c0964bfc9841b517d50fbc597de5d1
Juan Treminio <jtreminio@gmail.com>
2015-06-25 02:12:11 -0500

    Adds -O for "--omit-dir-times"

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sculpin generate --env=prod
-rsync -avze 'ssh -p 22' output_prod/ puphpet.com:/var/www/blog.puphpet.com/public_html
+rsync -Oavze 'ssh -p 22' output_prod/ puphpet.com:/var/www/blog.puphpet.com/public_html
